1c45ace66cc689d150dc25f0379a65d1ffac5a18
Martin Scotta <martinscotta@gmail.com>
2020-03-06 10:42:26 -0300

    Idea for about box
    (please dismiss this PR if the current about box looks as intended)
    
    This PR attempts to improve the about box, by ..
    
    1. adding space around the about title, which currently competes with the highlighted text on hierarchy
    2. moving the highlight text a bit below, to be more balanced with the box
    3. increase the about title on small screens, as it appears first there
    
    Here is how it currently looks:
    
    ![Current state](https://i.imgur.com/IO1Cbrg.png)
    
    and here is the current PR propsal:
    
    ![This proposal](https://i.imgur.com/Y2riYge.png)

--- components/about.vue
+++ components/about.vue
@@ -1,30 +1,46 @@
 <template>
     <div id="about" class="bg-blue-node">
         <div class="container center flex flex-wrap justify-between pt6 ph3 white">
-                        <div class="w-50-l w-100">
-                <div class="ph5-l pb5-l pa4 pt2 bg-white blue-node lh-copy nb5-l nb0">
-                    <h3 class="title-highlight ttu blue-node">about</h3>
-                <p class="mt0">Argentina is among the first international Node.js conferences taking place in Latin American territory, and a not-for-profit community effort to push Node.js forward in Argentina and Latin America as a whole.</p>
-
-<p class="mt0">This year’s conference will be a two–day event in sunny Buenos Aires, Argentina, held during October 9th & 10th. Both action-packed days will be comprised of internationally acclaimed, regional, and local speakers.
-
-</p>
+            <div class="w-50-l w-100">
+                <div class="ph5-l pb4-l pa4 pt2 bg-white blue-node lh-copy nb5-l nb0">
+                  <h3 class="title-highlight ttu blue-node">about</h3>
+                  <p class="mt0">
+                    Argentina is among the first international Node.js
+                    conferences taking place in Latin American territory,
+                    and a not-for-profit community effort to push Node.js
+                    forward in Argentina and Latin America as a whole.
+                  </p>
+                  <p class="mt0">
+                    This year’s conference will be a two–day event in
+                    sunny Buenos Aires, Argentina, held during October
+                    9th &amp; 10th. Both action-packed days will be comprised
+                    of internationally acclaimed, regional, and local speakers.
+                  </p>
                 </div>
             </div>
 
-            <div class="w-50-l w-100 ph5-l ph4 lh-copy pt0-l pv4">
-                <p class="mv0 highlight">We are expecting Noders, JavaScripters, front-enders, and other programmers from all over the world.</p>
-                
+            <div class="w-50-l w-100 ph5-l ph4 lh-copy pt0-l pv4 mt3-l">
+                <p class="mv0 highlight">
+                  We are expecting Noders, JavaScripters, front-enders,
+                  and other programmers from all over the world.
+                </p>
             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+
 .title-highlight {
+
+  font-size: 2.5em;
+  margin: 0;
+
+  @media screen and (max-width: 60em) {
     font-size: 3em;
-    margin: -1.7rem 0 0;
+  }
 }
+
 .highlight {
     font-size: 2.1em;
          @media screen and (max-width: 60em) {
